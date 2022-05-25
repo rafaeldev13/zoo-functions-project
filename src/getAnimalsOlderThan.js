333c652747494463e1bce42111ca955a00486c79
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-05-25 16:36:27 -0400

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species
+    .find((parametro) => parametro.name === animal)
+    .residents.every((idade) => idade.age >= age);
 }
+// console.log(getAnimalsOlderThan("lions", 12));
 
 module.exports = getAnimalsOlderThan;
