7f9043585dc27effffef6863fa9e0e9d6fdebc5d
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-05-25 17:15:09 -0400

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (typeof employeeName === 'undefined') {
+    return {};
+  }
+  const name = data.employees
+    .find((par) => par.firstName === employeeName || par.lastName === employeeName);
+  return name;
 }
 
 module.exports = getEmployeeByName;
