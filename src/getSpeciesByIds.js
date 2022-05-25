96d6e8937353beb81d206b5ae030a89d0f9f216e
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-05-25 15:30:35 -0400

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const onlyId = data.species;
+
+function getSpeciesByIds(...ids) {
+  if (!ids) {
+    return [];
+  }
+  const array = [];
+  ids.forEach((id) => {
+    const nome = onlyId.find((index) => index.id === id);
+    array.push(nome);
+  });
+  return array;
 }
 
 module.exports = getSpeciesByIds;
