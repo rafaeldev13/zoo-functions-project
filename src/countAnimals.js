c752d013e7c709662981c74f9630f4fef5575e12
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-05-25 22:33:28 -0400

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (typeof animal === 'undefined') {
+    const animais = {};
+    species.forEach((parametro) => {
+      animais[parametro.name] = parametro.residents.length;
+    });
+    return animais;
+  }
+  const { residents } = species.find((specie) => specie.name === animal.specie);
+
+  if (animal.sex) {
+    return residents.filter((resident) => resident.sex === animal.sex).length;
+  }
+  return residents.length;
 }
 
 module.exports = countAnimals;
