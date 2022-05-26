4a44ecde0d4b1f4524793ff50341dbaacce02a58
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-05-25 23:32:12 -0400

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
 
+  return { child, adult, senior };
 }
-
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entrance = countEntrants(entrants);
+  return entrance.child * 20.99 + entrance.adult * 49.99 + entrance.senior * 24.99;
 }
 
 module.exports = { calculateEntry, countEntrants };
