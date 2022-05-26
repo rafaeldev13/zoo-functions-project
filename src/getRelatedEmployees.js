ea70ca49d1de2bf447a7032bcb67c42299d3c687
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-05-25 21:48:46 -0400

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((gerente) =>
+    gerente.managers.find((element) => element === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    return employees.filter((funcionario) => funcionario.managers
+      .find((manager) => manager === managerId))
+      .map((funcionario) => `${funcionario.firstName} ${funcionario.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
