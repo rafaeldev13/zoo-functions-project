c35909ba13d892f8c32c8cf775518353a0fdb54e
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-05-31 10:09:41 -0400

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const result = [];
+  const employee = data.employees.find((parametro) => parametro.id === id);
+  const getFirstAnimal = data.species.find((firstAnimal) => firstAnimal
+    .id === employee.responsibleFor[0]);
+  const getOlderAnimal = getFirstAnimal.residents.sort((x, y) => y.age - x.age)[0];
+  result.push(getOlderAnimal.name);
+  result.push(getOlderAnimal.sex);
+  result.push(getOlderAnimal.age);
+  return result;
 }
 
 module.exports = getOldestFromFirstSpecies;
